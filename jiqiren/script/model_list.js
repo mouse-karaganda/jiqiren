ddc832c1e82f0a936fce245148565d1ebf2b6fa5
mouse-karaganda <natashaburnos@gmail.com>
2024-08-20 14:28:00 +0500

    Past version from 2024-08-20 14:28 Append models for Apitor Kit.

--- jiqiren/script/model_list.js
+++ jiqiren/script/model_list.js
@@ -434,277 +434,277 @@ app.ApitorKit.figures = [{
     number: 99,
     model: app.ApitorKit.robotX
 }, {
-    index: 99,
+    index: 50,
     folder: '67895cc57ae604b1e06a23bda8c36066',
-    cover: '8.png',
-    count: 99,
+    cover: '60.png',
+    count: 60,
     name: {rus: '', eng: ''},
     number: 99,
-    model: app.ApitorKit.robotE
+    model: app.ApitorKit.robotG
 }, {
-    index: 99,
+    index: 51,
     folder: '88623c8aa75be5f442924be3a6f0e373',
-    cover: '8.png',
-    count: 99,
+    cover: '33.png',
+    count: 35,
     name: {rus: '', eng: ''},
     number: 99,
     model: app.ApitorKit.robotE
 }, {
-    index: 99,
+    index: 52,
     folder: '304688ef132621e2690a64c4cddfd096',
-    cover: '8.png',
-    count: 99,
+    cover: '45.jpg',
+    count: 45,
     name: {rus: '', eng: ''},
     number: 99,
-    model: app.ApitorKit.robotE
+    model: app.ApitorKit.robotQ
 }, {
-    index: 99,
+    index: 53,
     folder: '871949bcb50c9f72d40bdc4ca3c2aa40',
-    cover: '8.png',
-    count: 99,
+    cover: '62.png',
+    count: 62,
     name: {rus: '', eng: ''},
     number: 99,
-    model: app.ApitorKit.robotE
+    model: app.ApitorKit.robotB
 }, {
-    index: 99,
+    index: 54,
     folder: '4829558b232bacdec578de4c5411c123',
-    cover: '8.png',
-    count: 99,
+    cover: '105.png',
+    count: 105,
     name: {rus: '', eng: ''},
     number: 99,
-    model: app.ApitorKit.robotE
+    model: app.ApitorKit.robotX
 }, {
-    index: 99,
+    index: 55,
     folder: '8775603575c7b835286fbbc491f49ae1',
-    cover: '8.png',
-    count: 99,
+    cover: '51.png',
+    count: 53,
     name: {rus: '', eng: ''},
     number: 99,
-    model: app.ApitorKit.robotE
+    model: app.ApitorKit.robotB
 }, {
-    index: 99,
+    index: 56,
     folder: '25331808001dbb7247eb469212b30e3c',
-    cover: '8.png',
-    count: 99,
+    cover: '56.png',
+    count: 56,
     name: {rus: '', eng: ''},
     number: 99,
-    model: app.ApitorKit.robotE
+    model: app.ApitorKit.robotQ
 }, {
-    index: 99,
+    index: 57,
     folder: 'a3f18dd13e5297aebfbc6213f825b504',
-    cover: '8.png',
-    count: 99,
+    cover: '75.png',
+    count: 75,
     name: {rus: '', eng: ''},
     number: 99,
-    model: app.ApitorKit.robotE
+    model: app.ApitorKit.robotJ
 }, {
-    index: 99,
+    index: 58,
     folder: 'a6bc17309126de2c3692e9c637640574',
-    cover: '8.png',
-    count: 99,
+    cover: '44.png',
+    count: 44,
     name: {rus: '', eng: ''},
     number: 99,
-    model: app.ApitorKit.robotE
+    model: app.ApitorKit.robotQ
 }, {
-    index: 99,
+    index: 59,
     folder: 'a8cec70c8ea69a3d7519fdbd32fb2809',
-    cover: '8.png',
-    count: 99,
+    cover: '107.png',
+    count: 107,
     name: {rus: '', eng: ''},
     number: 99,
-    model: app.ApitorKit.robotE
+    model: app.ApitorKit.robotX
 }, {
-    index: 99,
+    index: 60,
     folder: 'a12f14dca26bc4e642dae35016a907a9',
-    cover: '8.png',
-    count: 99,
+    cover: '51.png',
+    count: 51,
     name: {rus: '', eng: ''},
     number: 99,
-    model: app.ApitorKit.robotE
+    model: app.ApitorKit.robotG
 }, {
-    index: 99,
+    index: 61,
     folder: 'a804a85a94efe9dd26ac5403b1ddfc1a',
-    cover: '8.png',
-    count: 99,
+    cover: '50.png',
+    count: 52,
     name: {rus: '', eng: ''},
     number: 99,
-    model: app.ApitorKit.robotE
+    model: app.ApitorKit.robotB
 }, {
-    index: 99,
+    index: 62,
     folder: 'b1a4db45e1988c62d9f270a5efd40472',
-    cover: '8.png',
-    count: 99,
+    cover: '88.png',
+    count: 88,
     name: {rus: '', eng: ''},
     number: 99,
-    model: app.ApitorKit.robotE
+    model: app.ApitorKit.robotJ
 }, {
-    index: 99,
+    index: 63,
     folder: 'b9a0d93f96682c04f72b99c21389c112',
-    cover: '8.png',
-    count: 99,
+    cover: '37.png',
+    count: 39,
     name: {rus: '', eng: ''},
     number: 99,
     model: app.ApitorKit.robotE
 }, {
-    index: 99,
+    index: 64,
     folder: 'b70a09f8e0d3059b6e87763414c0b00c',
-    cover: '8.png',
-    count: 99,
+    cover: '47.png',
+    count: 49,
     name: {rus: '', eng: ''},
     number: 99,
     model: app.ApitorKit.robotE
 }, {
-    index: 99,
+    index: 65,
     folder: 'bb00e60f349c579ee61b16ca1b1b734c',
-    cover: '8.png',
-    count: 99,
+    cover: '50.jpg',
+    count: 50,
     name: {rus: '', eng: ''},
     number: 99,
-    model: app.ApitorKit.robotE
+    model: app.ApitorKit.robotQ
 }, {
-    index: 99,
+    index: 66,
     folder: 'bbab4cb2f44575a3e5240a7dd58d2605',
-    cover: '8.png',
-    count: 99,
+    cover: '84.png',
+    count: 84,
     name: {rus: '', eng: ''},
     number: 99,
-    model: app.ApitorKit.robotE
+    model: app.ApitorKit.robotJ
 }, {
-    index: 99,
+    index: 67,
     folder: 'c6ae8595c3cb87bd11f45d58e803a1d8',
-    cover: '8.png',
-    count: 99,
+    cover: '86.jpg',
+    count: 86,
     name: {rus: '', eng: ''},
     number: 99,
-    model: app.ApitorKit.robotE
+    model: app.ApitorKit.robotS
 }, {
-    index: 99,
+    index: 68,
     folder: 'c9dc93b87536b361cd00ea95d120beb9',
-    cover: '8.png',
-    count: 99,
+    cover: '52.jpg',
+    count: 52,
     name: {rus: '', eng: ''},
     number: 99,
-    model: app.ApitorKit.robotE
+    model: app.ApitorKit.robotQ
 }, {
-    index: 99,
+    index: 69,
     folder: 'c53da32011273a123e7d9f3c5d5a784c',
-    cover: '8.png',
-    count: 99,
+    cover: '83.jpg',
+    count: 83,
     name: {rus: '', eng: ''},
     number: 99,
-    model: app.ApitorKit.robotE
+    model: app.ApitorKit.robotS
 }, {
-    index: 99,
+    index: 70,
     folder: 'c51381fd5e4c04559c9cf89e0b65665e',
-    cover: '8.png',
-    count: 99,
+    cover: '85.jpg',
+    count: 85,
     name: {rus: '', eng: ''},
     number: 99,
-    model: app.ApitorKit.robotE
+    model: app.ApitorKit.robotS
 }, {
-    index: 99,
+    index: 71,
     folder: 'cacd969b906147914148f3524e1cf2d9',
-    cover: '8.png',
-    count: 99,
+    cover: '79.jpg',
+    count: 79,
     name: {rus: '', eng: ''},
     number: 99,
-    model: app.ApitorKit.robotE
+    model: app.ApitorKit.robotS
 }, {
-    index: 99,
+    index: 72,
     folder: 'cc46cab8c3755b93aa65db9989598f4e',
-    cover: '8.png',
-    count: 99,
+    cover: '86.jpg',
+    count: 86,
     name: {rus: '', eng: ''},
     number: 99,
-    model: app.ApitorKit.robotE
+    model: app.ApitorKit.robotS
 }, {
-    index: 99,
+    index: 73,
     folder: 'd9c450efff10e5d88db048edad188f32',
-    cover: '8.png',
-    count: 99,
+    cover: '56.png',
+    count: 56,
     name: {rus: '', eng: ''},
     number: 99,
-    model: app.ApitorKit.robotE
+    model: app.ApitorKit.robotX
 }, {
-    index: 99,
+    index: 74,
     folder: 'd15a7d6ff203a6ac533ae879dafba47d',
-    cover: '8.png',
-    count: 99,
+    cover: '39.png',
+    count: 41,
     name: {rus: '', eng: ''},
     number: 99,
     model: app.ApitorKit.robotE
 }, {
-    index: 99,
+    index: 75,
     folder: 'd15f031b6379d04401bab5ca119b16ea',
-    cover: '8.png',
-    count: 99,
+    cover: '59.png',
+    count: 59,
     name: {rus: '', eng: ''},
     number: 99,
-    model: app.ApitorKit.robotE
+    model: app.ApitorKit.robotX
 }, {
-    index: 99,
+    index: 76,
     folder: 'd60a88eeec4fe1452cdf11e6660d0a23',
-    cover: '8.png',
-    count: 99,
+    cover: '50.jpg',
+    count: 50,
     name: {rus: '', eng: ''},
     number: 99,
-    model: app.ApitorKit.robotE
+    model: app.ApitorKit.robotQ
 }, {
-    index: 99,
+    index: 77,
     folder: 'db8af41bbdf2bb8c55a52bbabfc0e498',
-    cover: '8.png',
-    count: 99,
+    cover: '83.jpg',
+    count: 83,
     name: {rus: '', eng: ''},
     number: 99,
-    model: app.ApitorKit.robotE
+    model: app.ApitorKit.robotS
 }, {
-    index: 99,
+    index: 78,
     folder: 'df23f42e6e3e8a2497ec6ed475764be0',
-    cover: '8.png',
-    count: 99,
+    cover: '68.png',
+    count: 68,
     name: {rus: '', eng: ''},
     number: 99,
-    model: app.ApitorKit.robotE
+    model: app.ApitorKit.robotR
 }, {
-    index: 99,
+    index: 79,
     folder: 'e215d69448f7399fc07284d52da0b7ce',
-    cover: '8.png',
-    count: 99,
+    cover: '38.jpg',
+    count: 38,
     name: {rus: '', eng: ''},
     number: 99,
-    model: app.ApitorKit.robotE
+    model: app.ApitorKit.robotQ
 }, {
-    index: 99,
+    index: 80,
     folder: 'e3721dda400e07588214ace56bbdd714',
-    cover: '8.png',
-    count: 99,
+    cover: '34.jpg',
+    count: 34,
     name: {rus: '', eng: ''},
     number: 99,
-    model: app.ApitorKit.robotE
+    model: app.ApitorKit.robotQ
 }, {
-    index: 99,
+    index: 81,
     folder: 'ea3c14e443d3e83766850ada7deef485',
-    cover: '8.png',
-    count: 99,
+    cover: '65.png',
+    count: 65,
     name: {rus: '', eng: ''},
     number: 99,
-    model: app.ApitorKit.robotE
+    model: app.ApitorKit.robotR
 }, {
-    index: 99,
+    index: 82,
     folder: 'f3a71420326023bb65cd2d29b3f18293',
-    cover: '8.png',
-    count: 99,
+    cover: '84.jpg',
+    count: 84,
     name: {rus: '', eng: ''},
     number: 99,
-    model: app.ApitorKit.robotE
+    model: app.ApitorKit.robotS
 }, {
-    index: 99,
+    index: 83,
     folder: 'fbc8c68e9bf0564e580ca9708a87beb5',
-    cover: '8.png',
-    count: 99,
+    cover: '56.jpg',
+    count: 56,
     name: {rus: '', eng: ''},
     number: 99,
-    model: app.ApitorKit.robotE
+    model: app.ApitorKit.robotQ
 }];
 
 app.SuperBot.figures = [{
