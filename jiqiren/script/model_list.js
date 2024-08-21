3b081101aee7693d9ea8e3be8461679d0c1ad8c9
mouse-karaganda <natashaburnos@gmail.com>
2024-08-21 21:52:00 +0500

    Past version from 2024-08-21 21:52 Put names for Apitor Kit.

--- jiqiren/script/model_list.js
+++ jiqiren/script/model_list.js
@@ -136,7 +136,15 @@ app.ApitorKit.figures = [{
     count: 166,
     name: {rus: 'Совместные модели SX', eng: 'SX Joint Model'},
     number: 11,
-    model: app.ApitorKit.robotS
+    model: app.ApitorKit.robotS, 
+}, {
+    index: 12,
+    folder: '6f88b00bfb9b159bb0c29c215d27aa51',
+    cover: '166.jpg',
+    count: 166,
+    name: {rus: 'Совместные модели SX', eng: 'SX Joint Model'},
+    number: 13,
+    model: app.ApitorKit.robotX
 }, {
     index: 13,
     folder: '7dc9aaa432cc827ed4df3555f37bf9dc',
@@ -198,8 +206,8 @@ app.ApitorKit.figures = [{
     folder: '10e9bf6fcc10239c38f86804f722c9c1',
     cover: '41.jpg',
     count: 41,
-    name: {rus: '', eng: ''},
-    number: 99,
+    name: {rus: 'Баскетбольная машина', eng: ''},
+    number: 11,
     model: app.ApitorKit.robotQ
 }, {
     index: 21,
@@ -230,7 +238,7 @@ app.ApitorKit.figures = [{
     folder: '31e2fd4650e4d9e77acc11dce1b1b376',
     cover: '66.jpg',
     count: 66,
-    name: {rus: 'Суперкар', eng: '--'},
+    name: {rus: 'Суперкар', eng: '--1'},
     number: 1,
     model: app.ApitorKit.robotS
 }, {
@@ -286,424 +294,432 @@ app.ApitorKit.figures = [{
     folder: '080beb181c219e55bfaf4f0de605e1ad',
     cover: '47.png',
     count: 47,
-    name: {rus: '', eng: ''},
-    number: 99,
+    name: {rus: 'Колесо обозрения', eng: ''},
+    number: 2,
     model: app.ApitorKit.robotG
 }, {
     index: 32,
     folder: '145d87b97b0805bf4222dfe813311e03',
     cover: '81.png',
     count: 81,
-    name: {rus: '', eng: ''},
-    number: 99,
+    name: {rus: 'Пусковая установка', eng: ''},
+    number: 6,
     model: app.ApitorKit.robotJ
 }, {
     index: 33,
     folder: '228f2b7a03efb41e6e849d1895b0b9a5',
     cover: '46.png',
     count: 46,
-    name: {rus: '', eng: ''},
-    number: 99,
+    name: {rus: 'Балет кроликов', eng: ''},
+    number: 7,
     model: app.ApitorKit.robotG
 }, {
     index: 34,
     folder: '311c66c884fe14c2c142acc844735fe5',
     cover: '160.jpg',
     count: 160,
-    name: {rus: '', eng: ''},
-    number: 99,
+    name: {rus: 'Совместные модели QX', eng: 'QX Joint Model'},
+    number: 21,
     model: app.ApitorKit.robotQ
+}, {
+    index: 34,
+    folder: '311c66c884fe14c2c142acc844735fe5',
+    cover: '160.jpg',
+    count: 160,
+    name: {rus: 'Совместные модели QX', eng: 'QX Joint Model'},
+    number: 14,
+    model: app.ApitorKit.robotX
 }, {
     index: 35,
     folder: '476e0af57e8b26868d8f3f93da4ed6f0',
     cover: '66.png',
     count: 64,
-    name: {rus: '', eng: ''},
-    number: 99,
+    name: {rus: 'Внедорожник', eng: ''},
+    number: 1,
     model: app.ApitorKit.robotR
 }, {
     index: 36,
     folder: '479e36d93748988adb64831d53083b65',
     cover: '59.png',
     count: 61,
-    name: {rus: '', eng: ''},
-    number: 99,
+    name: {rus: 'Самолётная пусковая установка', eng: ''},
+    number: 8,
     model: app.ApitorKit.robotB
 }, {
     index: 37,
     folder: '0548f0b03a5a68bc6b6902ccbd2e26f6',
     cover: '62.jpg',
     count: 62,
-    name: {rus: '', eng: ''},
-    number: 99,
+    name: {rus: 'Кукурузник', eng: ''},
+    number: 20,
     model: app.ApitorKit.robotQ
 }, {
     index: 38,
     folder: '585f3849407ab0014da6d5cde2a307f8',
     cover: '61.png',
     count: 63,
-    name: {rus: '', eng: ''},
-    number: 99,
+    name: {rus: 'Птеродактиль', eng: ''},
+    number: 2,
     model: app.ApitorKit.robotB
 }, {
     index: 39,
     folder: '818ee4aca576441e0600ecba04de7da1',
     cover: '50.png',
     count: 50,
-    name: {rus: '', eng: ''},
-    number: 99,
+    name: {rus: 'Барабанщик', eng: ''},
+    number: 1,
     model: app.ApitorKit.robotE
 }, {
     index: 40,
     folder: '849b9ac1c818c252fe4c31e71caaafac',
     cover: '54.png',
     count: 55,
-    name: {rus: '', eng: ''},
-    number: 99,
+    name: {rus: 'Монстр-кар', eng: ''},
+    number: 5,
     model: app.ApitorKit.robotB
 }, {
     index: 41,
     folder: '860b125ab9efc161b8ce99e956405fb8',
     cover: '102.png',
     count: 102,
-    name: {rus: '', eng: ''},
-    number: 99,
+    name: {rus: 'Динозавр', eng: 'Dinosaur'},
+    number: 5,
     model: app.ApitorKit.robotX
 }, {
     index: 42,
     folder: '893e7c267c26c01e15293bad66d92548',
     cover: '77.png',
     count: 77,
-    name: {rus: '', eng: ''},
-    number: 99,
+    name: {rus: 'Мототакси Тук-тук', eng: 'Tuk-Tuk'},
+    number: 3,
     model: app.ApitorKit.robotX
 }, {
     index: 43,
     folder: '0920fdd83c6139d18df056b89e33ef8f',
     cover: '66.png',
     count: 64,
-    name: {rus: '', eng: ''},
-    number: 99,
+    name: {rus: 'Бурильная машина', eng: ''},
+    number: 1,
     model: app.ApitorKit.robotJ
 }, {
     index: 44,
     folder: '965b4fc4129048bd2dee93dab4102d5e',
     cover: '41.png',
     count: 42,
-    name: {rus: '', eng: ''},
-    number: 99,
+    name: {rus: 'Ножничный подъёмник', eng: ''},
+    number: 9,
     model: app.ApitorKit.robotE
 }, {
     index: 45,
     folder: '6140e7ebe04b960b2383832ce16826ae',
     cover: '50.jpg',
     count: 50,
-    name: {rus: '', eng: ''},
-    number: 99,
+    name: {rus: 'Качели', eng: ''},
+    number: 12,
     model: app.ApitorKit.robotQ
 }, {
     index: 46,
     folder: '6679e611a8ac65b4038960cdd00f3d63',
     cover: '66.png',
     count: 66,
-    name: {rus: '', eng: ''},
-    number: 99,
+    name: {rus: 'Катапульта', eng: 'Catapult'},
+    number: 7,
     model: app.ApitorKit.robotX
 }, {
     index: 47,
     folder: '7510d0ab7da1cac23b35102aed3cfb4b',
     cover: '51.png',
     count: 53,
-    name: {rus: '', eng: ''},
-    number: 99,
+    name: {rus: 'Робот-собака', eng: ''},
+    number: 8,
     model: app.ApitorKit.robotE
 }, {
     index: 48,
     folder: '7516e7614af94def5436d489aaf1b9ca',
     cover: '47.png',
     count: 47,
-    name: {rus: '', eng: ''},
-    number: 99,
+    name: {rus: 'Цветок', eng: ''},
+    number: 8,
     model: app.ApitorKit.robotG
 }, {
     index: 49,
     folder: '056540d790ba5977a30486ea2ddf6ef9',
     cover: '85.png',
     count: 85,
-    name: {rus: '', eng: ''},
-    number: 99,
+    name: {rus: 'Цифровое пианино', eng: 'Digital Piano'},
+    number: 8,
     model: app.ApitorKit.robotX
 }, {
     index: 50,
     folder: '67895cc57ae604b1e06a23bda8c36066',
     cover: '60.png',
     count: 60,
-    name: {rus: '', eng: ''},
-    number: 99,
+    name: {rus: 'Пингвин', eng: ''},
+    number: 4,
     model: app.ApitorKit.robotG
 }, {
     index: 51,
     folder: '88623c8aa75be5f442924be3a6f0e373',
     cover: '33.png',
     count: 35,
-    name: {rus: '', eng: ''},
-    number: 99,
+    name: {rus: 'Весы', eng: ''},
+    number: 6,
     model: app.ApitorKit.robotE
 }, {
     index: 52,
     folder: '304688ef132621e2690a64c4cddfd096',
     cover: '45.jpg',
     count: 45,
-    name: {rus: '', eng: ''},
-    number: 99,
+    name: {rus: 'Клоун', eng: ''},
+    number: 16,
     model: app.ApitorKit.robotQ
 }, {
     index: 53,
     folder: '871949bcb50c9f72d40bdc4ca3c2aa40',
     cover: '62.png',
     count: 62,
-    name: {rus: '', eng: ''},
-    number: 99,
+    name: {rus: 'Боевой робот', eng: ''},
+    number: 6,
     model: app.ApitorKit.robotB
 }, {
     index: 54,
     folder: '4829558b232bacdec578de4c5411c123',
     cover: '105.png',
     count: 105,
-    name: {rus: '', eng: ''},
-    number: 99,
+    name: {rus: 'Робот', eng: 'Robot X'},
+    number: 2,
     model: app.ApitorKit.robotX
 }, {
     index: 55,
     folder: '8775603575c7b835286fbbc491f49ae1',
     cover: '51.png',
     count: 53,
-    name: {rus: '', eng: ''},
-    number: 99,
+    name: {rus: 'Бульдозер', eng: ''},
+    number: 3,
     model: app.ApitorKit.robotB
 }, {
     index: 56,
     folder: '25331808001dbb7247eb469212b30e3c',
     cover: '56.png',
     count: 56,
-    name: {rus: '', eng: ''},
-    number: 99,
+    name: {rus: 'Горилла', eng: ''},
+    number: 1,
     model: app.ApitorKit.robotQ
 }, {
     index: 57,
     folder: 'a3f18dd13e5297aebfbc6213f825b504',
     cover: '75.png',
     count: 75,
-    name: {rus: '', eng: ''},
-    number: 99,
+    name: {rus: 'Танк', eng: ''},
+    number: 3,
     model: app.ApitorKit.robotJ
 }, {
     index: 58,
     folder: 'a6bc17309126de2c3692e9c637640574',
     cover: '44.png',
     count: 44,
-    name: {rus: '', eng: ''},
-    number: 99,
+    name: {rus: 'Волчок', eng: ''},
+    number: 5,
     model: app.ApitorKit.robotQ
 }, {
     index: 59,
     folder: 'a8cec70c8ea69a3d7519fdbd32fb2809',
     cover: '107.png',
     count: 107,
-    name: {rus: '', eng: ''},
-    number: 99,
+    name: {rus: 'Монстр', eng: 'Monster'},
+    number: 9,
     model: app.ApitorKit.robotX
 }, {
     index: 60,
     folder: 'a12f14dca26bc4e642dae35016a907a9',
     cover: '51.png',
     count: 51,
-    name: {rus: '', eng: ''},
-    number: 99,
+    name: {rus: 'Единорог', eng: ''},
+    number: 1,
     model: app.ApitorKit.robotG
 }, {
     index: 61,
     folder: 'a804a85a94efe9dd26ac5403b1ddfc1a',
     cover: '50.png',
     count: 52,
-    name: {rus: '', eng: ''},
-    number: 99,
+    name: {rus: 'Трицератопс', eng: ''},
+    number: 4,
     model: app.ApitorKit.robotB
 }, {
     index: 62,
     folder: 'b1a4db45e1988c62d9f270a5efd40472',
     cover: '88.png',
     count: 88,
-    name: {rus: '', eng: ''},
-    number: 99,
+    name: {rus: 'Робот', eng: ''},
+    number: 2,
     model: app.ApitorKit.robotJ
 }, {
     index: 63,
     folder: 'b9a0d93f96682c04f72b99c21389c112',
     cover: '37.png',
     count: 39,
-    name: {rus: '', eng: ''},
-    number: 99,
+    name: {rus: 'Роботизированная рука', eng: ''},
+    number: 13,
     model: app.ApitorKit.robotE
 }, {
     index: 64,
     folder: 'b70a09f8e0d3059b6e87763414c0b00c',
     cover: '47.png',
     count: 49,
-    name: {rus: '', eng: ''},
-    number: 99,
+    name: {rus: 'Уборочная машина', eng: ''},
+    number: 10,
     model: app.ApitorKit.robotE
 }, {
     index: 65,
     folder: 'bb00e60f349c579ee61b16ca1b1b734c',
     cover: '50.jpg',
     count: 50,
-    name: {rus: '', eng: ''},
-    number: 99,
+    name: {rus: 'Подъёмный кран', eng: ''},
+    number: 7,
     model: app.ApitorKit.robotQ
 }, {
     index: 66,
     folder: 'bbab4cb2f44575a3e5240a7dd58d2605',
     cover: '84.png',
     count: 84,
-    name: {rus: '', eng: ''},
-    number: 99,
+    name: {rus: 'Робокар', eng: ''},
+    number: 4,
     model: app.ApitorKit.robotJ
 }, {
     index: 67,
     folder: 'c6ae8595c3cb87bd11f45d58e803a1d8',
     cover: '86.jpg',
     count: 86,
-    name: {rus: '', eng: ''},
-    number: 99,
+    name: {rus: 'Маятник', eng: 'Pendulum Ride'},
+    number: 8,
     model: app.ApitorKit.robotS
 }, {
     index: 68,
     folder: 'c9dc93b87536b361cd00ea95d120beb9',
     cover: '52.jpg',
     count: 52,
-    name: {rus: '', eng: ''},
-    number: 99,
+    name: {rus: 'Мотоцикл', eng: ''},
+    number: 8,
     model: app.ApitorKit.robotQ
 }, {
     index: 69,
     folder: 'c53da32011273a123e7d9f3c5d5a784c',
     cover: '83.jpg',
     count: 83,
-    name: {rus: '', eng: ''},
-    number: 99,
+    name: {rus: 'Слон', eng: 'Elephant'},
+    number: 2,
     model: app.ApitorKit.robotS
 }, {
     index: 70,
     folder: 'c51381fd5e4c04559c9cf89e0b65665e',
     cover: '85.jpg',
     count: 85,
-    name: {rus: '', eng: ''},
-    number: 99,
+    name: {rus: 'Кран-манипулятор', eng: '--4'},
+    number: 4,
     model: app.ApitorKit.robotS
 }, {
     index: 71,
     folder: 'cacd969b906147914148f3524e1cf2d9',
     cover: '79.jpg',
     count: 79,
-    name: {rus: '', eng: ''},
-    number: 99,
+    name: {rus: 'Коробка-сюрприз', eng: '--6'},
+    number: 6,
     model: app.ApitorKit.robotS
 }, {
     index: 72,
     folder: 'cc46cab8c3755b93aa65db9989598f4e',
     cover: '86.jpg',
     count: 86,
-    name: {rus: '', eng: ''},
-    number: 99,
+    name: {rus: 'Робот-пылесос', eng: 'Robot Vacuum'},
+    number: 7,
     model: app.ApitorKit.robotS
 }, {
     index: 73,
     folder: 'd9c450efff10e5d88db048edad188f32',
     cover: '56.png',
     count: 56,
-    name: {rus: '', eng: ''},
-    number: 99,
+    name: {rus: 'Чертёжная машина', eng: 'Drawing Machine'},
+    number: 10,
     model: app.ApitorKit.robotX
 }, {
     index: 74,
     folder: 'd15a7d6ff203a6ac533ae879dafba47d',
     cover: '39.png',
     count: 41,
-    name: {rus: '', eng: ''},
-    number: 99,
+    name: {rus: 'Пружинящая машина', eng: ''},
+    number: 2,
     model: app.ApitorKit.robotE
 }, {
     index: 75,
     folder: 'd15f031b6379d04401bab5ca119b16ea',
     cover: '59.png',
     count: 59,
-    name: {rus: '', eng: ''},
-    number: 99,
+    name: {rus: 'Скрипка', eng: 'Violin'},
+    number: 4,
     model: app.ApitorKit.robotX
 }, {
     index: 76,
     folder: 'd60a88eeec4fe1452cdf11e6660d0a23',
     cover: '50.jpg',
     count: 50,
-    name: {rus: '', eng: ''},
-    number: 99,
+    name: {rus: 'Качающаяся лодка', eng: ''},
+    number: 9,
     model: app.ApitorKit.robotQ
 }, {
     index: 77,
     folder: 'db8af41bbdf2bb8c55a52bbabfc0e498',
     cover: '83.jpg',
     count: 83,
-    name: {rus: '', eng: ''},
-    number: 99,
+    name: {rus: 'Боксёр', eng: 'Boxer'},
+    number: 10,
     model: app.ApitorKit.robotS
 }, {
     index: 78,
     folder: 'df23f42e6e3e8a2497ec6ed475764be0',
     cover: '68.png',
     count: 68,
-    name: {rus: '', eng: ''},
-    number: 99,
+    name: {rus: 'Грузовик', eng: ''},
+    number: 3,
     model: app.ApitorKit.robotR
 }, {
     index: 79,
     folder: 'e215d69448f7399fc07284d52da0b7ce',
     cover: '38.jpg',
     count: 38,
-    name: {rus: '', eng: ''},
-    number: 99,
+    name: {rus: 'Автоматические двери', eng: ''},
+    number: 15,
     model: app.ApitorKit.robotQ
 }, {
     index: 80,
     folder: 'e3721dda400e07588214ace56bbdd714',
     cover: '34.jpg',
     count: 34,
-    name: {rus: '', eng: ''},
-    number: 99,
+    name: {rus: 'Лифт', eng: ''},
+    number: 19,
     model: app.ApitorKit.robotQ
 }, {
     index: 81,
     folder: 'ea3c14e443d3e83766850ada7deef485',
     cover: '65.png',
     count: 65,
-    name: {rus: '', eng: ''},
-    number: 99,
+    name: {rus: 'Робот-жук', eng: ''},
+    number: 4,
     model: app.ApitorKit.robotR
 }, {
     index: 82,
     folder: 'f3a71420326023bb65cd2d29b3f18293',
     cover: '84.jpg',
     count: 84,
-    name: {rus: '', eng: ''},
-    number: 99,
+    name: {rus: 'Самолёт', eng: 'Airplane'},
+    number: 9,
     model: app.ApitorKit.robotS
 }, {
     index: 83,
     folder: 'fbc8c68e9bf0564e580ca9708a87beb5',
     cover: '56.jpg',
     count: 56,
-    name: {rus: '', eng: ''},
-    number: 99,
+    name: {rus: 'Гоночная машина', eng: ''},
+    number: 3,
     model: app.ApitorKit.robotQ
 }];
 
